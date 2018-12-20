0d7d3d0026bbd231403f9b055f85693bd3b57398
Diego Restrepo <restrepo@udea.edu.co>
2018-12-20 14:40:02 -0500

    lc

---
+++ revista_new.php
@@ -0,0 +1,14 @@
+DOI (and hit ENTER):
+<form>
+<input name="doi" type="text" />
+</form>
+
+<?php
+  if ($_GET["doi"]){     
+    $command = escapeshellcmd('./revista_new.py '.$_GET["doi"]);
+    $output = shell_exec($command);
+    echo $output;
+
+  }
+//phpinfo();
+?>
